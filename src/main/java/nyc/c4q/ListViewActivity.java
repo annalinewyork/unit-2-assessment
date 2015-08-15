7509404416582149f1a359fedbaf7224d42e953c
annalinewyork <annalinewyork@gmail.com>
2015-08-14 22:34:29 -0400

    test7

--- src/main/java/nyc/c4q/ListViewActivity.java
+++ src/main/java/nyc/c4q/ListViewActivity.java
@@ -1,7 +1,15 @@
 package nyc.c4q;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ListViewActivity extends Activity {
@@ -19,11 +27,52 @@ public class ListViewActivity extends Activity {
             "#bf538d"
     };
     public TextView textLog;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
         textLog = (TextView) findViewById(R.id.textLog);
+        listView = (ListView) findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, COLORS);
+        listView.setAdapter(adapter);
+
+        //08
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition = position;
+                String color = (String) listView.getItemAtPosition(position);
+
+                textLog.setText("You clicked on Item(position=" + itemPosition + "," + "color=" + color + ")");
+            }
+        });
     }
+
+    //07
+    public int getCount() {
+        return COLORS.length;
+    }
+
+    public class ColorAdapter extends ArrayAdapter<String>{
+
+        public ColorAdapter(Context context, int resource) {
+            super(context, resource);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View rowView = super.getView(position, convertView, parent);
+            ColorDrawable colorDrawable = new ColorDrawable();
+            colorDrawable.setColor(Integer.parseInt(COLORS[position]));
+          //  ColorDrawable backgroundColor = Color.parseColor(COLORS[position]);
+            rowView.setBackgroundColor(backgroundColor);
+            return rowView;
+        }
+    }
+
+
+
 }
